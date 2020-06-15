fdf510c0f6e781054d7061fdaef98e4d43ef70ce
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-06-15 20:16:23 +0200

    Fix: Review frontend restart method in deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,6 +7,6 @@ set -eu
 set -o pipefail
 cd huelladigital-platform
 docker-compose pull
-docker-compose up frontend -d
+docker-compose restart frontend
 exit 0
 """ | ssh -t -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no master@${env}.huelladigital.ayudadigital.org bash -
