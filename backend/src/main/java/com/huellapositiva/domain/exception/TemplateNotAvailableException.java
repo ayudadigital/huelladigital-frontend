daf2a2ea6320c81e82bc87b374d2bff16fe224e7
Pedro Amador Rodríguez <pedroamador.rodriguez@gmail.com>
2020-06-15 20:08:53 +0200

    Upgrade: Remove backend part of the inherit repository (#1)
    * Update: Remove backend part
    
    * Build: Review Jenkinsfile removing backend part
    
    * Update: Restart only the frontend service in the deploy

--- backend/src/main/java/com/huellapositiva/domain/exception/TemplateNotAvailableException.java
+++
@@ -1,7 +0,0 @@
-package com.huellapositiva.domain.exception;
-
-public class TemplateNotAvailableException extends RuntimeException {
-    public TemplateNotAvailableException(Exception s) {
-        super(s);
-    }
-}
