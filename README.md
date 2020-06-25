c1e16565d8a17c50b0b6163520a47393e82cef63
Ruben Zagon <ruben.zagon@gmail.com>
2020-06-25 12:47:11 +0100

    New: add environment variables (#2)
    * Refactor extract the urls con Client class util
    
    * Define environment variables
    
    * Update documentation
    
    * Fixit test e2e
    
    * Refactor erase unused code
    
    * Try to fix CI problems with ROUTES rename
    
    * Try to fix CI problems with routes in lowercase rename

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Huella Digital
 
-Plataforma huellapositiva.com de gestion de voluntariado.
+Plataforma huellapositiva.com de gestión de voluntariado.
 
-Es un projecto liderado por la Fundacion [Soy Mamut](https://soymamut.com) y desarrollado por voluntarios de la comunidad [Ayuda Digital](https://ayudadigital.org)
+Es un proyecto liderado por la Fundación [Soy Mamut](https://soymamut.com) y desarrollado por voluntarios de la comunidad [Ayuda Digital](https://ayudadigital.org)
 
 ## Backend
 
-Tiene su propio [README.md](backend/readme) en la carpeta `backend`
+Tiene su propio [README.md](https://github.com/ayudadigital/huelladigital-backend/blob/develop/backend/README.md) en la carpeta `backend`
 
 ## Frontend
 
-Tiene su propio [README.md](backend/readme) en la carpeta `frontend`
+Tiene su propio [README.md](frontend/README.md) en la carpeta `frontend`
--- frontend/src/components/organisms/Forms/FormUtils/client.ts
+++ frontend/src/components/organisms/Forms/FormUtils/client.ts
@@ -1,9 +1,10 @@
+import { BASE } from '../../../../utils/base';
 import { ROUTE } from '../../../../utils/routes';
+
 
 export default class Client {
   registerVolunteer(credentials: object) {
-    // FIXME: extract the URL to a variable on ENV i guess?
-    const URL = 'http://localhost:8080/api/v1/volunteers';
+    const URL = `${BASE.API}${ROUTE.API.volunteers.register}`;
     fetch(URL, {
       method: 'POST',
       headers: {
@@ -12,12 +13,13 @@ export default class Client {
       body: JSON.stringify(credentials),
     })
       .then((response) => {
-        if (response.status === 201) {
-          // FIXME: extract the URL to a variable on ENV i guess?
-          window.location.replace(`http://localhost:3000${ROUTE.email.confirmation}`);
+        if (response.status === 201 || response.status === 200) {
+          window.location.replace(`${BASE.URI}${ROUTE.email.confirmation}`);
         }
       })
       // tslint:disable-next-line:no-console
       .catch((error) => console.log(error));
   }
 }
+
+
