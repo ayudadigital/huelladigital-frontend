4b47e4d90f65aa7f61433f1df4f2481c7b99de3e
Rubén Zagon <ruben.zagon@gmail.com>
2020-06-29 12:24:01 +0100

    Add message user already exist

--- frontend/src/components/organisms/Forms/FormUtils/client.ts
+++ frontend/src/components/organisms/Forms/FormUtils/client.ts
@@ -1,25 +1,27 @@
 import { BASE } from '../../../../utils/base';
 import { ROUTE } from '../../../../utils/routes';
 
-
 export default class Client {
   registerVolunteer(credentials: object) {
     const URL = `${BASE.API}${ROUTE.API.volunteers.register}`;
-    fetch(URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(credentials),
-    })
-      .then((response) => {
-        if (response.status === 201 || response.status === 200) {
-          window.location.replace(`${BASE.URI}${ROUTE.email.confirmation}`);
-        }
+    return (
+      fetch(URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(credentials),
       })
-      // tslint:disable-next-line:no-console
-      .catch((error) => console.log(error));
+        .then((response) => {
+          if (response.status === 201 || response.status === 200) {
+            window.location.replace(`${BASE.URI}${ROUTE.email.confirmation}`);
+            return 200;
+          } else if (response.status === 409) {
+            return 409;
+          }
+        })
+        // tslint:disable-next-line:no-console
+        .catch((error) => console.log(error))
+    );
   }
 }
-
-
