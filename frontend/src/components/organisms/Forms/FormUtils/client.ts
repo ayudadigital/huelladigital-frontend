ca99f237af32d88a33a34cfe6478edf97dce4a84
Rubén Zagon <ruben.zagon@gmail.com>
2020-06-29 20:39:54 +0100

    Refactor class Client

--- frontend/src/components/organisms/Forms/FormUtils/client.ts
+++ frontend/src/components/organisms/Forms/FormUtils/client.ts
@@ -3,9 +3,9 @@ import { ROUTE } from '../../../../utils/routes';
 
 
 export default class Client {
-  registerVolunteer(credentials: object) {
+  async registerVolunteer(credentials: object) {
     const URL = `${BASE.API}${ROUTE.API.volunteers.register}`;
-    return fetch(URL, {
+    return await fetch(URL, {
       method: 'POST',
       mode: 'cors',
       headers: {
@@ -15,9 +15,10 @@ export default class Client {
     })
       .then((response) => {
         if (response.status === 201 || response.status === 200) {
-          JSON.stringify(response.json().then(res => {
-            window.document.cookie = `accessToken=${res.accessToken}; path=/;`;
-            window.document.cookie = `refreshToken=${res.refreshToken}; path=/;`;
+
+          JSON.stringify(response.json().then((res: { accessToken: string, refreshToken: string }) => {
+            setCookie('accessToken', res.accessToken);
+            setCookie('refreshToken', res.refreshToken);
           }));
           window.location.replace(`${BASE.URI}${ROUTE.email.confirmation}`);
         } else if (response.status === 409) {
@@ -27,4 +28,16 @@ export default class Client {
       // tslint:disable-next-line:no-console
       .catch((error) => console.log(error));
   }
+}
+
+
+function setCookie(key: string, value: any) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+  document.cookie = `${key}=${value}; expires=${expires.toUTCString()}; path=/;`;
+}
+
+function getCookie(key: string) {
+  const keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+  return keyValue ? keyValue[2] : null;
 }
