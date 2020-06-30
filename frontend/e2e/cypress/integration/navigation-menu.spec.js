ff2ff8f30d40ef6d4db45fe0edcfd50311abef76
Rubén Zagon <ruben.zagon@gmail.com>
2020-06-30 18:16:23 +0100

    Add test e2e  of navigation menu

--- frontend/e2e/cypress/integration/navigation-menu.spec.js
+++ frontend/e2e/cypress/integration/navigation-menu.spec.js
@@ -23,40 +23,48 @@ describe('Navigation menu', () => {
 
   it.only('verify render of menu mobil', () => {
 
+    // lets see what our app looks like on a super small screen
     cy.viewport(320, 480);
-
-    cy.get('a.LinkText')
-      .should('be', false);
-
-
-
-    // the navbar should have collapse since our screen is smaller
-
+    cy.get('a.LinkText').should('be', false);
 
     // lets see what our app looks like on a super large screen
     cy.viewport(2999, 2999);
+    cy.get('a.LinkText').should('be', true);
 
     // the change otherwise it is a little too fast to see :)
-
-    cy.viewport('macbook-13');
-    cy.wait(200);
-
     cy.viewport('ipad-2');
     cy.wait(200);
+    cy.get('.container').click();
+    cy.contains('Inicio').should('contain', 'Inicio').click({ force: true });
+    cy.url().should('include', ROUTE.home);
+
     cy.viewport('ipad-mini');
     cy.wait(200);
-    cy.viewport('iphone-6+');
+    cy.get('.container').click();
+    cy.contains('Acceder').should('contain', 'Acceder').click({ force: true });
+    cy.url().should('include', ROUTE.loginRegister);
 
+
+    cy.viewport('iphone-6+');
     cy.wait(200);
+    cy.get('.container').click();
+    cy.contains('Inicio').should('contain', 'Inicio').click({ force: true });
+    cy.url().should('include', ROUTE.home);
+
     cy.viewport('iphone-3');
     cy.wait(200);
+    cy.get('.container').click();
+    cy.contains('Acceder').should('contain', 'Acceder').click({ force: true });
+    cy.url().should('include', ROUTE.loginRegister);
 
-    // cy.viewport() accepts an orientation for all presets
+
     // the default orientation is 'portrait'
-    cy.viewport('ipad-2', 'portrait');
-    cy.wait(200);
+    // cy.viewport('ipad-2', 'portrait');
+    // cy.wait(200);
     cy.viewport('iphone-4', 'landscape');
     cy.wait(200);
+    cy.get('.container').click();
+    cy.contains('Inicio').should('contain', 'Inicio').click({ force: true });
+    cy.url().should('include', ROUTE.home);
   });
-
 });
