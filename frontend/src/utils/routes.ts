44acd8e79decbf002c71e24eaddc47d0c9f68044
Airán <airanschez0@gmail.com>
2020-07-07 10:42:00 +0100

    Added new routes for convocatories

--- frontend/src/utils/routes.ts
+++ frontend/src/utils/routes.ts
@@ -2,7 +2,8 @@ type typeRoute = {
   home: string;
   email: { confirmation: string; resendConfirmation: string };
   loginRegister: string;
-  API: { volunteers: { register: string }, email: { resendConfirmation: string } };
+  API: { volunteers: { register: string }; email: { resendConfirmation: string } };
+  convocatory: { convocatories: { list: string; register: string; details: string } };
 };
 
 const V1 = 'v1';
@@ -10,6 +11,13 @@ const ENDPOINT_VOLUNTEERS = `/api/${V1}/volunteers`;
 const ENDPOINT_EMAILCONFIRMATION = `/api/${V1}/email-confirmation`;
 
 export const ROUTE: typeRoute = {
+  convocatory: {
+    convocatories: {
+      details: '/details',
+      list: '/list',
+      register: '/register',
+    },
+  },
   home: '/',
   email: {
     confirmation: '/email-confirmation-register',
