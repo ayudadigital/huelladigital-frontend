369c48b051d92e07a8ca1a1a751a0d47914e9227
Rubén Zagon <ruben.zagon@gmail.com>
2020-07-18 17:54:39 +0100

    🐛 Fix the modal on menu nav

--- frontend/src/components/organisms/Header/Header.tsx
+++ frontend/src/components/organisms/Header/Header.tsx
@@ -5,29 +5,37 @@ import { Image } from '../../atoms/Image';
 import { NavBarDesktop } from '../../molecules/NavBarDesktop';
 import { Context } from '../../../Context';
 import { HamburguerMenu } from '../../molecules/HamburguerMenu';
+import useWindowSize from '../../../utils/hooks/useWindowSize';
 
 export const Header: React.FC<{}> = () => {
-    const [showModal, setShowModal] = useState(false);
-    const auth = useContext(Context);
+  const [showModal, setShowModal] = useState(false);
 
-    function handleModal() {
-        setShowModal(!showModal);
-    }
+  const auth = useContext(Context);
+
+  const size = useWindowSize();
+
+  function handleModal() {
+    setShowModal(!showModal);
+  }
 
 
-    return (
-        <div className="Header">
-            <Image
-                source="https://huellapositiva.com/wp-content/uploads/2020/03/cropped-Logo-Huella-Positiva-PV-05.png"
-                description="logo"
-            />
-            {
-                // @ts-ignore
-                <NavBarDesktop isAuth={auth.isAuth} show={showModal} onClick={handleModal} onClickDisconnect={auth.removeAuth}/>
-            }
-            <HamburguerMenu onClick={handleModal} show={showModal}/>
-        </div>
-    );
+  return (
+    <div className="Header">
+      <Image
+        source="https://huellapositiva.com/wp-content/uploads/2020/03/cropped-Logo-Huella-Positiva-PV-05.png"
+        description="logo"
+      />
+      {
+        // @ts-ignore
+        size.width < 992
+        // @ts-ignore
+          ? <NavBarDesktop isAuth={auth.isAuth} show={showModal} onClick={handleModal} onClickDisconnect={auth.removeAuth}/>
+        // @ts-ignore
+          : <NavBarDesktop isAuth={auth.isAuth} show={showModal} onClickDisconnect={auth.removeAuth}/>
+      }
+      <HamburguerMenu onClick={handleModal} show={showModal}/>
+    </div>
+  );
 };
 
 Header.displayName = 'Header';
