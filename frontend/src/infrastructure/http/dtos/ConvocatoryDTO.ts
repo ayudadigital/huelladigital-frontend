ece951c0b2a58c7bb36e107d09e3f63269a6eedf
Javier Díaz Domínguez <javidiazdom@gmail.com>
2020-11-11 11:21:08 +0000

    :coffee: New convocatory DTO

--- frontend/src/infrastructure/http/dtos/ConvocatoryDTO.ts
+++ frontend/src/infrastructure/http/dtos/ConvocatoryDTO.ts
@@ -1,2 +1,36 @@
 // This is a placeholder file
-export {};
+export interface ConvocatoryDto {
+    id: string;
+    title: string;
+    esalName: string;
+    province: string;
+    town: string;
+    address: string;
+    minimumAge: number;
+    maximumAge: number;
+    startingProposalDate: string;
+    startingVolunteeringDate: string;
+    closingProposalDate: string;
+    status: string;
+    description: string;
+    instructions: string;
+    extraInfo: string;
+    durationInDays: string;
+    category: string;
+    imageURL: string;
+    inscribedVolunteers: [VolunteerDto];
+    inscribedVolunteersCount: 0;
+    skills: [SkillDto];
+    requirements: [
+        string
+    ];
+}
+
+export interface SkillDto {
+    name: string;
+    description: string;
+}
+
+export interface VolunteerDto {
+    id: string;
+}
