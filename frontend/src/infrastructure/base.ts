f5cca7c29ba0cfff19ce252f05a0a2608b460ff6
Javier Díaz Domínguez <javidiazdom@gmail.com>
2020-11-11 12:31:00 +0000

    :traffic_light: Added development proxy for backend API requests

--- frontend/src/infrastructure/base.ts
+++ frontend/src/infrastructure/base.ts
@@ -3,7 +3,7 @@ const BASE_URI = process.env.REACT_APP_BASE_URI
   : `http://localhost:3000`;
 const BASE_API = process.env.REACT_APP_BASE_API
   ? `${process.env.REACT_APP_BASE_API}`
-  : `http://localhost:8080`;
+  : `http://localhost:3000`;
 export const BASE = {
   URI: BASE_URI,
   API: BASE_API,
