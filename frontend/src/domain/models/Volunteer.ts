9d53b6d2f234cbd6c69b123b720ee3548120ae15
Javier Díaz Domínguez <47884739+javidiazdom@users.noreply.github.com>
2020-11-19 18:26:44 +0000

    First models (#15)
    Add domain models and services for ESALs, convocatories and volunteers.

--- frontend/src/domain/models/Volunteer.ts
+++ frontend/src/domain/models/Volunteer.ts
@@ -1,2 +1,15 @@
-// This is a placeholder file
-export {};
+export type Volunteer = {
+  name?: string;
+  surname?: string;
+  username?: string;
+  telephoneNumber?: string;
+  birthDate?: string;
+  email: string;
+  profile: string;
+  photoURL: string;
+  twitterAccount: string;
+  linkedInAccount: string;
+  instagramAccount: string;
+  cv: string;
+  refreshPasswordToken: string;
+};
