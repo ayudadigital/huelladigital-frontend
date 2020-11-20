ee113154700f5442be071db032b7d0083c69fd72
Samuel <55623374+Oiranca@users.noreply.github.com>
2020-11-20 09:43:15 +0000

    Crendial model and login logic (#17)
    Created credential model, volunteer login service, volunteer repository and its http implementation.

--- frontend/src/infrastructure/components/organisms/Forms/FormLoginVolunteer/FormLoginVolunteer.tsx
+++ frontend/src/infrastructure/components/organisms/Forms/FormLoginVolunteer/FormLoginVolunteer.tsx
@@ -2,33 +2,30 @@ import React, { useCallback, useEffect, useState } from 'react';
 import '../styles.scss';
 import { FieldForm } from '../../../molecules/FieldForm';
 import { SubmitButton } from '../../../atoms/SubmitButton';
+import { VolunteerCredential } from '../../../../../domain/models/Credential';
+import { VolunteerService } from '../../../../../domain/services/Volunteer.service';
 
 export const FormLoginVolunteer: React.FC<any> = () => {
   const [stateButton, setStateButton] = useState(true);
   const [messageShow, setMessageShow] = useState(false);
-  const [data, setData] = useState(
-    {
-      email: '',
-      password: '',
-    },
-  );
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  });
 
   const handleStateButton = useCallback(() => {
-    (data.email !== '' && data.password !== '')
+    data.email !== '' && data.password !== ''
       ? setStateButton(false)
       : setStateButton(true);
   }, [data.email, data.password]);
 
   async function handleSubmit(event: React.FormEvent) {
     event.preventDefault();
-    const volunteerDTO = {
+    const volunteerCredential: VolunteerCredential = {
       email: data.email,
       password: data.password,
     };
-
-    /*if (response === 403) {
-      setMessageShow(true);
-    }*/
+    VolunteerService.loginVolunteer(volunteerCredential);
   }
 
   useEffect(() => {
@@ -37,16 +34,22 @@ export const FormLoginVolunteer: React.FC<any> = () => {
 
   return (
     <form className="ContainerForm" method="POST" id="form" onSubmit={handleSubmit}>
-      <FieldForm title={'Email'}
-                 type={'email'}
-                 name={'email'}
-                 onChange={(e) => setData({ ...data, email: e.target.value })}/>
-      <FieldForm title={'Contraseña'}
-                 type={'password'}
-                 name={'password'}
-                 onChange={(e) => setData({ ...data, password: e.target.value })}/>
-      {messageShow && <p style={{color: 'red', marginTop: '5px'}}>El email o contraseña están mal</p>}
-      <SubmitButton text={'Acceder'} disabled={stateButton}/>
+      <FieldForm
+        title={'Email'}
+        type={'email'}
+        name={'email'}
+        onChange={(e) => setData({ ...data, email: e.target.value })}
+      />
+      <FieldForm
+        title={'Contraseña'}
+        type={'password'}
+        name={'password'}
+        onChange={(e) => setData({ ...data, password: e.target.value })}
+      />
+      {messageShow && (
+        <p style={{ color: 'red', marginTop: '5px' }}>El email o contraseña están mal</p>
+      )}
+      <SubmitButton text={'Acceder'} disabled={stateButton} />
     </form>
   );
 };
--- frontend/src/infrastructure/repositories/Volunteer.repository.ts
+++ frontend/src/infrastructure/repositories/Volunteer.repository.ts
@@ -5,38 +5,51 @@ import { BASE } from '../base';
 import { activateAuth } from '../http/cookies';
 
 const login = (loginCredentials: VolunteerCredentialsDTO) => {
-    http.post(`${BASE.API}${ROUTE.API.volunteers.login}`,JSON.stringify(loginCredentials)).then((response) => {
-        if (response.status === 201 || response.status === 200) {
-            JSON.stringify(response.json().then((res: { accessToken: string, refreshToken: string }) => {
-                activateAuth(res.accessToken, res.refreshToken);
-            }));
-            window.location.replace(`${BASE.URI}${ROUTE.home}`);
+  http
+    .post(`${BASE.API}${ROUTE.API.volunteers.login}`, JSON.stringify(loginCredentials))
+    .then((response) => {
+      if (response.status === 201 || response.status === 200) {
+        JSON.stringify(
+          response.json().then((res: { accessToken: string; refreshToken: string }) => {
+            activateAuth(res.accessToken, res.refreshToken);
+          }),
+        );
+        window.location.replace(`${BASE.URI}${ROUTE.home}`);
 
-            return 200;
-        } else {
-            return 403;
-        }
+        return 200;
+      } else {
+        return 403;
+      }
     })
-    .catch((error) => {console.log(error)});
-
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 const register = (registerCredentials: VolunteerCredentialsDTO) => {
-    http.post(`${BASE.API}${ROUTE.API.volunteers.register}`,JSON.stringify(registerCredentials)).then((response) => {
-        if (response.status === 201 || response.status === 200) {
-
-          JSON.stringify(response.json().then((res: { accessToken: string, refreshToken: string }) => {
+  http
+    .post(
+      `${BASE.API}${ROUTE.API.volunteers.register}`,
+      JSON.stringify(registerCredentials),
+    )
+    .then((response) => {
+      if (response.status === 201 || response.status === 200) {
+        JSON.stringify(
+          response.json().then((res: { accessToken: string; refreshToken: string }) => {
             activateAuth(res.accessToken, res.refreshToken);
-          }));
-          window.location.replace(`${BASE.URI}${ROUTE.email.confirmation}`);
-        } else if (response.status === 409) {
-          return 409;
-        }
+          }),
+        );
+        window.location.replace(`${BASE.URI}${ROUTE.email.confirmation}`);
+      } else if (response.status === 409) {
+        return 409;
+      }
     })
-    .catch((error) => {console.log(error)});
-}
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 export const volunteerRepository = {
-    login,
-    register
+  login,
+  register,
 };
