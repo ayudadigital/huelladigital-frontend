93d0d10a1ccc89796c89543d8f8ce14b945bf934
Javier Díaz Domínguez <javidiazdom@gmail.com>
2020-11-26 13:14:33 +0000

    :art: Started working on convocatory details css

--- frontend/src/infrastructure/view/pages/Convocations/ConvocatoryDetails/ConvocatoryDetails.tsx
+++ frontend/src/infrastructure/view/pages/Convocations/ConvocatoryDetails/ConvocatoryDetails.tsx
@@ -2,14 +2,18 @@ import * as React from 'react';
 import './ConvocatoryDetails.scss';
 import {Convocatory} from '../../../../../domain/models/Convocatory';
 import exampleConvocatory from './exampleConvocatory.json';
+import {Image} from '../../../components/atoms/Image';
 
 export const ConvocatoryDetails: React.FC<{}> = () => {
   const convocatory = exampleConvocatory.Convocatory as Convocatory;
   return (
     <div className="ConvocatoryDetails">
-      <div className="img-title-container">
-        <div className="title">
-          <h2>{convocatory.title}</h2>
+      <div className="Convocatory">
+        <div className="Img-title-container">
+          <Image source={convocatory.imageURL} description=""></Image>
+          <div className="Title">
+            <h2>{convocatory.title}</h2>
+          </div>
         </div>
       </div>
     </div>
