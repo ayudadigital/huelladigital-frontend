131e3ad054854c9fc6cba73770f1882eca541831
Javier Díaz Domínguez <javidiazdom@gmail.com>
2020-11-26 14:15:07 +0000

    :new: Logic for convocatory representation

--- frontend/src/infrastructure/view/pages/Convocations/ConvocatoryDetails/ConvocatoryDetails.tsx
+++ frontend/src/infrastructure/view/pages/Convocations/ConvocatoryDetails/ConvocatoryDetails.tsx
@@ -1,11 +1,41 @@
 import * as React from 'react';
 import './ConvocatoryDetails.scss';
-import {Convocatory} from '../../../../../domain/models/Convocatory';
+import {Convocatory,Skill} from '../../../../../domain/models/Convocatory';
 import exampleConvocatory from './exampleConvocatory.json';
 import {Image} from '../../../components/atoms/Image';
 
 export const ConvocatoryDetails: React.FC<{}> = () => {
+  const listRequirements = (requirements: string[]) => {
+    let result = '<ul>';
+    requirements.forEach((requirement) => {
+      result += '<ul>' + requirement + '</ul>';
+    });
+    return result + '</ul>';
+  }
+  const listSkills = (skills: Skill[]) => {
+    let result = '<ul>';
+    skills.forEach(skill => {
+      result += '<li>'+ skill.name+': '+skill.description+'</li>';
+    });
+    return result + '</ul>';
+  };
   const convocatory = exampleConvocatory.Convocatory as Convocatory;
+  const sections = [{
+    question: '¿Qué voy a hacer?',
+    text: convocatory.description
+  },
+  {
+    question: '¿Qué habilidades desarrollaré para mi CV?',
+    text: listSkills(convocatory.skills)
+  },
+  {
+    question: '¿Qué necesito?',
+    text: listRequirements(convocatory.requirements),
+  },
+  {
+    question: '¿Qué más necesito saber',
+    text: convocatory.extraInfo
+  }];
   return (
     <div className="ConvocatoryDetails">
       <div className="Convocatory">
@@ -18,6 +48,14 @@ export const ConvocatoryDetails: React.FC<{}> = () => {
         <div className="Content">
           <div className="TableContainer">
             {/** Table component **/}
+          </div>
+          <div className="ConvocatoryInfo">
+            {sections.map((section)=> {
+              return <>
+              <h3>{section.question}</h3>
+              <p>{section.text}</p>
+              </>;
+            })}
           </div>
         </div>
       </div>
