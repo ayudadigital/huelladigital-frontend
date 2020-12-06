d68149f1254ace8fb2b48ad2248eade937abcfc5
Samuel Romero Arbelo <oiranca@gmail.com>
2020-12-06 21:06:46 +0000

    Fixed a attribute in story file

--- frontend/src/infrastructure/view/components/atoms/InputTextArea/InputTextArea.stories.tsx
+++ frontend/src/infrastructure/view/components/atoms/InputTextArea/InputTextArea.stories.tsx
@@ -7,4 +7,4 @@ export default {
   decorators: [withA11y],
 };
 
-export const withText = () => <InputTextArea id="" placeholder="Text area" />;
+export const withText = () => <InputTextArea name="Input Text Area" placeholder="Input Text area" />;
