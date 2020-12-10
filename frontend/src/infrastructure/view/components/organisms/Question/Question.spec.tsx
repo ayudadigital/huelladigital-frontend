d5c3c800d757f70110a587f79f516d695255bba2
Javier Díaz Domínguez <javidiazdom@gmail.com>
2020-12-10 09:52:48 +0000

    :wrench: Solved tests problems

--- frontend/src/infrastructure/view/components/organisms/Question/Question.spec.tsx
+++ frontend/src/infrastructure/view/components/organisms/Question/Question.spec.tsx
@@ -3,18 +3,10 @@ import { render, RenderResult } from '@testing-library/react';
 import { Question} from '.';
 
 describe('Question', () => {
-  it('should display the default message', () => {
-<<<<<<< HEAD
+  it('should display the passed question', () => {
     const renderResult: RenderResult = render(
-      <Question title = "" body = ""/>,
-    );
-    expect(renderResult.queryByText('Hello from Question!')).toBeTruthy();
-=======
-    const title = 'Pregunta';
-    const renderResult: RenderResult = render(
-      <Question title = {title} body = ""/>,
+      <Question title = "Pregunta" body = "Respuesta"/>,
     );
     expect(renderResult.queryByText('Pregunta')).toBeTruthy();
->>>>>>> 6fab1db5629103258ff190607108597500c53e3e
   });
 });
