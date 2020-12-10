c04c9199628e4b341880de71424b9ddd8b2f998d
Javier Díaz Domínguez <javidiazdom@gmail.com>
2020-12-10 13:16:18 +0000

    :wrench: Fixed default test

--- frontend/src/infrastructure/view/components/molecules/ConvocatoryInformationDisplay/ConvocatoryInformationDisplay.spec.tsx
+++ frontend/src/infrastructure/view/components/molecules/ConvocatoryInformationDisplay/ConvocatoryInformationDisplay.spec.tsx
@@ -3,10 +3,10 @@ import { render, RenderResult } from '@testing-library/react';
 import { ConvocatoryInformationDisplay} from './';
 
 describe('ConvocatoryInformationDisplay', () => {
-  it('should display the default message', () => {
+  it('should display the default message when no convocatory provided', () => {
     const renderResult: RenderResult = render(
       <ConvocatoryInformationDisplay/>,
     );
-    expect(renderResult.queryByText('Hello from ConvocatoryInformationDisplay!')).toBeTruthy();
+    expect(renderResult.queryByText('No information available')).toBeTruthy();
   });
 });
