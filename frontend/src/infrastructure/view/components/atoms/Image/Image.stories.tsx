0fc6817816fada7d9fc1ed3184b3dd2ea9ff5dd3
Samuel AQ <43220418+Samuel-AQ@users.noreply.github.com>
2021-01-07 14:50:54 +0100

    Home view has been updated following the current design of the web (#46)
    * Home structure has been made
    
    * Descriptive image molecule created
    
    * Descriptive image component updated
    
    * Home view has beed updated
    
    * Some residual code has been removed

--- frontend/src/infrastructure/view/components/atoms/Image/Image.stories.tsx
+++ frontend/src/infrastructure/view/components/atoms/Image/Image.stories.tsx
@@ -11,14 +11,23 @@ export default {
   decorators: [withA11y, withKnobs],
 };
 
-export const Logo = () => <Image source={logo} description={'logo'} width={text('Ancho', '100px')}/>;
+export const Logo = () => (
+  <Image source={logo} description={'logo'} width={text('Ancho', '100px')} />
+);
 export const SuperHeroes = () => {
   return (
     <Fragment>
-      <Image source={superHeroes} description={'super heroes logo register'}/>
-      <Image source={superHeroes} description={'super heroes logo register'} width={'300px'}/>
-      <Image source={superHeroes} description={'super heroes logo register'} width={'10%'}/>
+      <Image source={superHeroes} description={'super heroes logo register'} />
+      <Image
+        source={superHeroes}
+        description={'super heroes logo register'}
+        width={'300px'}
+      />
+      <Image
+        source={superHeroes}
+        description={'super heroes logo register'}
+        width={'10%'}
+      />
     </Fragment>
-
   );
 };
