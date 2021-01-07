e9ed622cb26138a4f93bd68152c847b1fce32da5
Samuel <55623374+Oiranca@users.noreply.github.com>
2021-01-07 14:52:20 +0100

    Fixed form register esal (#48)
    * Modified div tag to forms tag into FormRegisterEsal and FormRegisterContactPerson
    
    * Style into MixFromRegisterEsal
    
    * Corrected some names and styles. And modified some components
    
    * Deleted unnecessary components  and fixed some problems with the tests
    
    * Removed code {' '} into FormRegisterEsal

--- frontend/src/infrastructure/view/components/organisms/Forms/FormRegisterEsal/FormRegisterEsal.tsx
+++ frontend/src/infrastructure/view/components/organisms/Forms/FormRegisterEsal/FormRegisterEsal.tsx
@@ -6,6 +6,8 @@ import { FormRadio } from '../../../molecules/FormRadio';
 import { Label } from '../../../atoms/Label';
 import { Image } from '../../../atoms/Image';
 import superHeroes from '../../../atoms/Image/assets/superHeroes.svg';
+import { LinkText } from '../../../atoms/LinkText';
+import { SubmitButton } from '../../../atoms/SubmitButton';
 
 export const FormRegisterEsal: React.FC<{}> = () => {
   const island = [
@@ -26,38 +28,77 @@ export const FormRegisterEsal: React.FC<{}> = () => {
     'Federación Deportiva',
   ];
 
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="register-esal">
-      <header>
-        <h1>Datos Entidad</h1>
-      </header>
-      <section>
-        <div className={'row'}>
-          <div className={'col'}>
-            <FieldForm title={'Nombre Entidad *'} type={'text'} name={'Entidad'} />
+    <form className="FormRegisterEsal" method={'POST'} id="form" onSubmit={handleSubmit}>
+      <div className={'register-esal'}>
+        <div className="div-register-contact-person">
+          <header>
+            <h1>Persona de Contacto</h1>
+          </header>
+          <div className="row">
+            <div className={'col'}>
+              <FieldForm title={'Nombre *'} type={'text'} name={'Nombre'} />
+            </div>
+            <div className={'col'}>
+              <FieldForm title={'Apellidos *'} type={'text'} name={'Apellidos'} />
+            </div>
           </div>
-          <div className={'col'}>
-            <FieldForm title={'Página Web'} type={'text'} name={'Web'} />
+          <div className="row">
+            <div className={'col'}>
+              <FieldForm title={'Email *'} type={'email'} name={'Email'} />
+            </div>
+            <div className={'col'}>
+              <FieldForm
+                title={'Confirmar email *'}
+                type={'email'}
+                name={'ConfirmarEmail'}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className={'col'}>
+              <FieldForm
+                title={'Teléfono de contacto *'}
+                type={'text'}
+                name={'Telefono'}
+              />
+            </div>
+            <div className={'col'}>
+              <FieldForm title={'Contraseña *'} type={'password'} name={'Password'} />
+            </div>
           </div>
         </div>
-      </section>{' '}
-      <section>
-        <div className={'row'}>
-          <div className={'col'}>
-            <TextAreaForm
-              title={'Descripción *'}
-              name={'Descripcion'}
-              placeholder={'Breve descripción de la entidad'}
-              rows={10}
-              cols={2}
-            />
+        <div className={'div-register-esal'}>
+          <header>
+            <h1>Datos Entidad</h1>
+          </header>
+          <div className={'row'}>
+            <div className={'col'}>
+              <FieldForm title={'Nombre entidad *'} type={'text'} name={'Entidad'} />
+            </div>
+            <div className={'col'}>
+              <FieldForm title={'Página web'} type={'text'} name={'Web'} />
+            </div>
           </div>
-          <div className={'col localization'}>
-            <Label text={'Ubicación *'} />
-            <div className={'row'}>
-              <div className={'col'}>
-                <section>
-                  <div className="form-radio">
+          <div className={'row'}>
+            <div className={'col'}>
+              <TextAreaForm
+                title={'Descripción *'}
+                name={'Descripcion'}
+                placeholder={'Breve descripción de la entidad'}
+                rows={10}
+                cols={2}
+              />
+            </div>
+            <div className={'col localization'}>
+              <Label text={'Ubicación *'} />
+              <div className={'row'}>
+                <div className={'col'}>
+                  <div className="div-radio">
                     {island.map((islands, index) => {
                       return (
                         <FormRadio
@@ -71,73 +112,105 @@ export const FormRegisterEsal: React.FC<{}> = () => {
                       );
                     })}
                   </div>
-                </section>
-              </div>
-              <div className={'col postal-code'}>
-                <FieldForm title={'Código Postal *'} type={'text'} name={'Otros'} />
+                </div>
+                <div className={'col postal-code'}>
+                  <FieldForm title={'Código postal *'} type={'text'} name={'Otros'} />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-      <div className={'row'}>
-        <div className={'col'}>
-          <Label text={'Logo *'} />
-          <div className="image-upload">
-            <label>
-              <input type="file" className={'file'} />
-              <Image
-                source={superHeroes}
-                description={'super heroes logo register'}
-                width={'300px'}
-              />
-            </label>
-          </div>
-        </div>
-        <div className={'col organization'}>
-          <Label text={'Tipo de entidad *'} />
-          <div className="row">
-            <div className="form-organization">
-              {associationType.map((types, index) => {
-                return (
+          <div className={'row'}>
+            <div className={'col'}>
+              <Label text={'Logo *'} />
+              <div className="image-upload">
+                <label>
+                  <input type="file" className={'file'} />
+                  <Image
+                    source={superHeroes}
+                    description={'super heroes logo register'}
+                    width={'300px'}
+                  />
+                </label>
+              </div>
+            </div>
+            <div className={'col organization'}>
+              <Label text={'Tipo de entidad *'} />
+              <div className="row">
+                <div className="div-organization">
+                  {associationType.map((types, index) => {
+                    return (
+                      <FormRadio
+                        title={''}
+                        type={'radio'}
+                        name={types}
+                        value={types}
+                        checked={false}
+                        key={index}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+              <div className={'register-government'}>
+                <Label
+                  text={
+                    'Registrada como entidad de voluntariado en el Gobierno de Canarias *'
+                  }
+                />
+                <div className={'row'}>
                   <FormRadio
                     title={''}
-                    type={'radio'}
-                    name={types}
-                    value={types}
+                    type={'checkbox'}
+                    name={'Affirmative'}
+                    value={'Sí'}
                     checked={false}
-                    key={index}
                   />
-                );
-              })}
+                  <FormRadio
+                    title={''}
+                    type={'checkbox'}
+                    name={'Negative'}
+                    value={'No'}
+                    checked={false}
+                  />
+                </div>
+              </div>
             </div>
           </div>
-          <div className={'register-government'}>
-            <Label
-              text={
-                'Registrada como entidad de voluntariado en el Gobierno de Canarias *'
-              }
-            />
-            <div className={'row'}>
+          <div className={'row consents-check'}>
+            <Label text={'Consentimientos *'} />
+            <div className={'data-protection'}>
               <FormRadio
-                title={''}
+                title={'Política Privacidad'}
                 type={'checkbox'}
-                name={'Affirmative'}
-                value={'Sí'}
+                name={'Política Privacidad'}
+                value={''}
                 checked={false}
               />
+              <p>
+                Estoy de acuerdo de con la {''}
+                <LinkText to={'/'} text={'Política de privacidad'} />.
+              </p>
+            </div>
+            <div className={'data-protection'}>
               <FormRadio
-                title={''}
+                title={'Protección de datos'}
                 type={'checkbox'}
-                name={'Negative'}
-                value={'No'}
+                name={'Proteccion de datos'}
+                value={''}
                 checked={false}
               />
+              <p>
+                Estoy de acuerdo de con la {''}
+                <LinkText to={'/'} text={'Política de protección de datos'} />.
+              </p>
+            </div>
+            <div className={'button-register'}>
+              <SubmitButton text={'Registrar Entidad'} />
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
