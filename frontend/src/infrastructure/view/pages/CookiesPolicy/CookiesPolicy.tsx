4fb8087a2c8dc7e0c7c36981c29f2bc8115ce908
Osama Moussati Lamhamdi <66735418+blueC4P@users.noreply.github.com>
2021-02-01 08:39:58 +0000

    CoockiesPolicy and LegalAdvice improved (#57)
    * Add links in LegalAdvice component and improve style in LegalAdvice.scss
    
    * Improve responsive design of LegalAdvice
    
    * Improve CoockiesPolicy
    
    * Switch classNames in CoockiesPolicy to simple cammel case to distinguish from component

--- frontend/src/infrastructure/view/pages/CookiesPolicy/CookiesPolicy.tsx
+++ frontend/src/infrastructure/view/pages/CookiesPolicy/CookiesPolicy.tsx
@@ -6,34 +6,32 @@ import { ReactComponent as Cookie } from './assets/cookie.svg';
 export const CookiesPolicy: React.FC<{}> = () => (
   <div className="CookiesPolicy">
     <h1>Política de cookies</h1>
-    <div className="FirstRow">
+    <div className="firstRow">
       <Cookie />
-      <div>
+      <p>
         Una cookie es un pequeño fichero de texto que se almacena en su navegador cuando visita casi cualquier página web. Su utilidad es que la web sea capaz de recordar su visita cuando vuelva a navegar por esa página. Las cookies suelen almacenar información de carácter técnico, preferencias personales, personalización de contenidos, estadísticas de uso, enlaces a redes sociales, acceso a cuentas de usuario, etc. El objetivo de la cookie es adaptar el contenido de la web a su perfil y necesidades, sin cookies los servicios ofrecidos por cualquier página se verían mermados notablemente. Si desea consultar más información sobre qué son las cookies, qué almacenan, cómo eliminarlas, desactivarlas, etc., <LinkText to="https://soymamut.com/mas-informacion-sobre-las-cookies/" text="le rogamos se dirija a este enlace." />
-      </div>
+      </p>
     </div>
-    <div className="OtherRows">
+    <div className="otherRows">
       <h2>Cookies utilizadas en esta web</h2>
-      <div>
+        <p>
         Siguiendo las directrices de la Agencia Española de Protección de Datos procedemos a detallar el uso de cookies que hace esta web con el fin de informarle con la máxima exactitud posible.
-
-      <h3>Cookies propias</h3>
-        <ul>
-          <li>
-            <b>Cookies de sesión,</b> para garantizar que los usuarios que escriban comentarios en el blog sean humanos y no aplicaciones automatizadas. De esta forma se combate el spam.
-          </li>
-        </ul>
-
-        <h3>Cookies de terceros</h3>
-        <ul>
-          <li>
-            <b>Google Analytics:</b> Almacena cookies para poder elaborar estadísticas sobre el tráfico y volumen de visitas de esta web. Al utilizar este sitio web está consintiendo el tratamiento de información acerca de usted por Google. Por tanto, el ejercicio de cualquier derecho en este sentido deberá hacerlo comunicando directamente con Google.
-          </li>
-          <li>
-            <b>Redes sociales:</b> Cada red social utiliza sus propias cookies para que usted pueda pinchar en botones del tipo Me gusta o Compartir.
-          </li>
-        </ul>
-      </div>
+        </p>
+        <h3>Cookies propias</h3>
+          <ul>
+            <li>
+              <b>Cookies de sesión,</b> para garantizar que los usuarios que escriban comentarios en el blog sean humanos y no aplicaciones automatizadas. De esta forma se combate el spam.
+            </li>
+          </ul>
+          <h3>Cookies de terceros</h3>
+          <ul>
+            <li>
+              <b>Google Analytics:</b> Almacena cookies para poder elaborar estadísticas sobre el tráfico y volumen de visitas de esta web. Al utilizar este sitio web está consintiendo el tratamiento de información acerca de usted por Google. Por tanto, el ejercicio de cualquier derecho en este sentido deberá hacerlo comunicando directamente con Google.
+            </li>
+            <li>
+              <b>Redes sociales:</b> Cada red social utiliza sus propias cookies para que usted pueda pinchar en botones del tipo Me gusta o Compartir.
+            </li>
+          </ul>
     </div>
     <div className="OtherRows">
       <h2>
