c2ec5cd4dcb67c111f207e56fee648103bd700b6
Samuel Arrocha Quevedo <samuelaq96@gmail.com>
2021-02-03 10:57:59 +0000

    profile model has been created

---
+++ frontend/src/domain/models/Profile.ts
@@ -0,0 +1,17 @@
+export type Profile = {
+  name: string;
+  surname: string;
+  email: string;
+  province: string;
+  zipCode: string;
+  address: string;
+  island: string;
+  photoURL?: string;
+  twitter?: string;
+  linkedIn?: string;
+  instagram?: string;
+  cv?: string;
+  telephoneNumber?: string;
+  birthDate: string;
+  additionalInformation?: string;
+};
