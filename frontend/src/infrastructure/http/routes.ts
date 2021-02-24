a0e1e01bbd4407b90cf8ee95705610c6b6a49bb7
blueC4P <osamag4g@gmail.com>
2021-02-24 12:22:57 +0000

    nothing

--- frontend/src/infrastructure/http/routes.ts
+++ frontend/src/infrastructure/http/routes.ts
@@ -61,70 +61,70 @@ const ENDPOINT_CONTACTPERSON = `api/${V1}/contactperson`;
 const ENDPOINT_RECOVERYPASSWORD = `api/${V1}/handling-password`;
 
 export const ROUTE: typeRoute = {
-         proposals: {
-           details: '/proposal-details',
-           list: '/propopsals-list',
-           register: '/proposal-register',
-         },
-         organizations: {
-           register: '/organization-register',
-         },
-         home: '/',
-         email: {
-           confirmation: '/email-confirmation-register',
-           resendConfirmation: '/resend-email-confirmation',
-         },
-         cookiesPolicy: '/cookies-policy',
-         loginRegister: '/login-register',
-         faq: '/frequently-asked-questions',
-         legalAdvice: '/legal-advice',
-         privacyPolicy: '/privacy-policy',
-         volunteers: {
-           profile: '/profile',
-         },
-         API: {
-           volunteers: {
-             register: `${ENDPOINT_VOLUNTEERS}`,
-             login: `${ENDPOINT_AUTHENTICATION}/login`,
-             uploadCv: `${ENDPOINT_VOLUNTEERS}/profile/cv`,
-             uploadPhoto: `${ENDPOINT_VOLUNTEERS}/profile/photo`,
-             profile: `${ENDPOINT_VOLUNTEERS}/profile`,
-           },
-           email: {
-             resendConfirmation: `${ENDPOINT_EMAILCONFIRMATION}/resend-email-confirmation`,
-             confirmEmail: (hash: string) => `${ENDPOINT_EMAILCONFIRMATION}/${hash}`,
-           },
-           proposals: {
-             register: `${ENDPOINT_PROPOSALS}`,
-             fetchProposal: (id: number) => `${ENDPOINT_PROPOSALS}/${id}`,
-             joinProposal: (id: number) => `${ENDPOINT_PROPOSALS}/${id}/join`,
-             registerAsReviser: `${ENDPOINT_PROPOSALS}/reviser`,
-             fetchListOfProposals: (page: number, size: number) =>
-               `${ENDPOINT_PROPOSALS}/${page}/${size}`,
-             fetchListOfProposalsAsReviser: (page: number, size: number) =>
-               `${ENDPOINT_PROPOSALS}/${page}/${size}/reviser`,
-             submitRevision: (id: number) => `${ENDPOINT_PROPOSALS}/revision/${id}`,
-             fetchVolunteers: (id: number) => `${ENDPOINT_PROPOSALS}/${id}/volunteers`,
-             fetchProposalsAndVolunteers: (id: number) =>
-               `${ENDPOINT_PROPOSALS}/${id}/proposal`,
-             cancelProposal: (id: number) => `${ENDPOINT_PROPOSALS}/${id}/cancel`,
-             changeStatusOfVolunteerInProposal: `${ENDPOINT_PROPOSALS}/changeStatusVolunteerProposal`,
-           },
-           organizations: {
-             register: `${ENDPOINT_ORGANIZATIONS}`,
-             delete: (id: number) => `${ENDPOINT_ORGANIZATIONS}/${id}`,
-             registerAsReviser: `${ENDPOINT_ORGANIZATIONS}/reviser`,
-             registerEsalEmployee: `${ENDPOINT_CONTACTPERSON}`,
-             duda: (id: number) => `${ENDPOINT_CONTACTPERSON}/${id}`,
-           },
-           unLoggedUsers: {
-             sendEmailForRecoveringPassword: `${ENDPOINT_EMAILCONFIRMATION}/sendRecoveryPasswordEmail`,
-             updatePassword: (hash: string) =>
-               `${ENDPOINT_EMAILCONFIRMATION}/changePassword/${hash}`,
-           },
-           users: {
-             updatePassword: `${ENDPOINT_EMAILCONFIRMATION}/editPassword`,
-           },
-           tokens: { refresh: `/api/${V1}/refresh` },
-         },
-       };
+  proposals: {
+    details: '/proposal-details',
+    list: '/propopsals-list',
+    register: '/proposal-register',
+  },
+  organizations: {
+    register: '/organization-register',
+  },
+  home: '/',
+  email: {
+    confirmation: '/email-confirmation-register',
+    resendConfirmation: '/resend-email-confirmation',
+  },
+  cookiesPolicy: '/cookies-policy',
+  loginRegister: '/login-register',
+  faq: '/frequently-asked-questions',
+  legalAdvice: '/legal-advice',
+  privacyPolicy: '/privacy-policy',
+  volunteers: {
+    profile: '/profile',
+  },
+  API: {
+    volunteers: {
+      register: `${ENDPOINT_VOLUNTEERS}`,
+      login: `${ENDPOINT_AUTHENTICATION}/login`,
+      uploadCv: `${ENDPOINT_VOLUNTEERS}/profile/cv`,
+      uploadPhoto: `${ENDPOINT_VOLUNTEERS}/profile/photo`,
+      profile: `${ENDPOINT_VOLUNTEERS}/profile`,
+    },
+    email: {
+      resendConfirmation: `${ENDPOINT_EMAILCONFIRMATION}/resend-email-confirmation`,
+      confirmEmail: (hash: string) => `${ENDPOINT_EMAILCONFIRMATION}/${hash}`,
+    },
+    proposals: {
+      register: `${ENDPOINT_PROPOSALS}`,
+      fetchProposal: (id: number) => `${ENDPOINT_PROPOSALS}/${id}`,
+      joinProposal: (id: number) => `${ENDPOINT_PROPOSALS}/${id}/join`,
+      registerAsReviser: `${ENDPOINT_PROPOSALS}/reviser`,
+      fetchListOfProposals: (page: number, size: number) =>
+        `${ENDPOINT_PROPOSALS}/${page}/${size}`,
+      fetchListOfProposalsAsReviser: (page: number, size: number) =>
+        `${ENDPOINT_PROPOSALS}/${page}/${size}/reviser`,
+      submitRevision: (id: number) => `${ENDPOINT_PROPOSALS}/revision/${id}`,
+      fetchVolunteers: (id: number) => `${ENDPOINT_PROPOSALS}/${id}/volunteers`,
+      fetchProposalsAndVolunteers: (id: number) =>
+        `${ENDPOINT_PROPOSALS}/${id}/proposal`,
+      cancelProposal: (id: number) => `${ENDPOINT_PROPOSALS}/${id}/cancel`,
+      changeStatusOfVolunteerInProposal: `${ENDPOINT_PROPOSALS}/changeStatusVolunteerProposal`,
+    },
+    organizations: {
+      register: `${ENDPOINT_ORGANIZATIONS}`,
+      delete: (id: number) => `${ENDPOINT_ORGANIZATIONS}/${id}`,
+      registerAsReviser: `${ENDPOINT_ORGANIZATIONS}/reviser`,
+      registerEsalEmployee: `${ENDPOINT_CONTACTPERSON}`,
+      duda: (id: number) => `${ENDPOINT_CONTACTPERSON}/${id}`,
+    },
+    unLoggedUsers: {
+      sendEmailForRecoveringPassword: `${ENDPOINT_EMAILCONFIRMATION}/sendRecoveryPasswordEmail`,
+      updatePassword: (hash: string) =>
+        `${ENDPOINT_EMAILCONFIRMATION}/changePassword/${hash}`,
+    },
+    users: {
+      updatePassword: `${ENDPOINT_EMAILCONFIRMATION}/editPassword`,
+    },
+    tokens: { refresh: `/api/${V1}/refresh` },
+  },
+};
