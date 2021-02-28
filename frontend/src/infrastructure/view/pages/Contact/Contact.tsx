b2ef0a0831b0f92de93a6b0d5c688813f857f162
Samuel Arrocha Quevedo <samuelaq96@gmail.com>
2021-02-28 22:12:57 +0000

    Info content css update

--- frontend/src/infrastructure/view/pages/Contact/Contact.tsx
+++ frontend/src/infrastructure/view/pages/Contact/Contact.tsx
@@ -1,38 +1,46 @@
 import React from 'react';
+import { ROUTE } from '../../../http/routes';
 import { LinkButton } from '../../components/atoms/LinkButton/LinkButton';
+import { LinkText } from '../../components/atoms/LinkText';
 import { FieldForm } from '../../components/molecules/FieldForm';
 import { FormSelect } from '../../components/molecules/FormSelect';
+import './Contact.scss';
 
 export const Contact: React.FC<{}> = () => {
   return (
     <div className="main-div">
       <div className="info-div">
         <p className="title">¡No dudes en ponerte en contacto con nosotros!</p>
-        <ul>
-          <li>
-            <b>Persona en busca de ayuda:</b> Has identificado alguna necesidad y/o
-            necesitas voluntarios que te ayuden.
-          </li>
-          <li>
-            <b>Ayuda Profesional:</b> Si eres un profesional o empresa y has detectado
-            alguna posible mejora con la que crees que puedes contribuir en esta campaña,
-            nos vendría genial el apoyo de especialistas en: copywriting, marketing,
-            diseño gráfico, redes sociales, comunicación, creación de contenidos, ley de
-            protección de datos… así como en las diferentes iniciativas ciudadanas que
-            irán surgiendo en los próximos días.
-          </li>
-          <li>
-            <b>Prensa / Comunicación:</b> Si quieres ayudar difundiendo esta iniciativa,
-            puedes hacerlo a través de las redes sociales, mediante una colaboración en
-            prensa o medios de comunicación.
-          </li>
-          <li>
-            <b>Asesoramiento:</b> Si tienes en mente poner en marcha una iniciativa de
-            ayuda y no sabes como empezar, necesitas contactos o… quizá podamos ayudarte.
-          </li>
-        </ul>
+        <div>
+          <ul>
+            <li>
+              <b>Persona en busca de ayuda:</b> has identificado alguna necesidad y/o
+              necesitas voluntarios que te ayuden.
+            </li>
+            <li>
+              <b>Ayuda Profesional:</b> si eres un profesional o empresa y has detectado
+              alguna posible mejora con la que crees que puedes contribuir en esta
+              campaña, nos vendría genial el apoyo de especialistas en: copywriting,
+              marketing, diseño gráfico, redes sociales, comunicación, creación de
+              contenidos, ley de protección de datos… así como en las diferentes
+              iniciativas ciudadanas que irán surgiendo en los próximos días.
+            </li>
+            <li>
+              <b>Prensa / Comunicación:</b> si quieres ayudar difundiendo esta iniciativa,
+              puedes hacerlo a través de las redes sociales, mediante una colaboración en
+              prensa o medios de comunicación.
+            </li>
+            <li>
+              <b>Asesoramiento:</b> si tienes en mente poner en marcha una iniciativa de
+              ayuda y no sabes como empezar, necesitas contactos o… quizá podamos
+              ayudarte.
+            </li>
+          </ul>
+        </div>
+
         <p className="faq">
-          Si tienes alguna duda consulta nuestras preguntas frecuentes.
+          Si tienes alguna duda consulta nuestras{' '}
+          <LinkText to={ROUTE.faq} text="preguntas frecuentes" />
         </p>
         <p className="slogan">¡Cada persona suma!</p>
       </div>
