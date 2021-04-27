90ebd95d8b7b27882bba5fc076c5c62efa567490
Samuel Romero Arbelo <oiranca@gmail.com>
2021-04-27 11:49:10 +0100

    refactor:modified ESAL.repository.ts and Esal.service.ts for insert contact person data and service

--- frontend/src/domain/services/Esal.service.ts
+++ frontend/src/domain/services/Esal.service.ts
@@ -1,28 +1,33 @@
-import { Esal, EsalEmployee } from '../models/Esal';
-import { ESALRepository } from '../../infrastructure/repositories/ESAL.repository';
-import { activateAuth } from '../../infrastructure/http/cookies';
+import {Esal, EsalEmployee} from '../models/Esal';
+import {ESALRepository} from '../../infrastructure/repositories/ESAL.repository';
+import {activateAuth} from '../../infrastructure/http/cookies';
 
-const regsiterBunch = (esal: Esal) => {
-  // ESALRepository.registerEsalEmployee(esalEmployee).then((response: any) => {
-  //   if (response.status === 201) {
-  //     JSON.stringify(
-  //       response
-  //         .json()
-  //         .then((res: { accessToken: string; refreshToken: string; roles: string }) => {
-  //           activateAuth(res.accessToken, res.refreshToken, res.roles[0]);
-  //           console.log(res);
-  //           ESALRepository.registerEsal(esal).then((response: any) => {
-  //             if (response.status === 201) {
-  //               //Añadir logo si existe
-  //               //Routing (¿Qué pagina se muestra a continuación?)
-  //             }
-  //           });
-  //         }),
-  //     );
-  //   }
-  // });
+const registerEsal = async (esal: Esal) => {
+    await ESALRepository.registerEsal(esal).then((response: any) => {
+        if (response.status === 201) {
+            console.log(response);
+            // Añadir logo si existe
+            // Routing (¿Qué pagina se muestra a continuación?)
+        }
+    });
+};
+
+const registerBunch = async (esal: Esal, newEmployee: EsalEmployee) => {
+    await ESALRepository.registerEmployee(newEmployee).then((response: any) => {
+        if (response.status === 201) {
+            JSON.stringify(
+                response
+                    .json()
+                    .then((res: { accessToken: string; refreshToken: string; roles: string }) => {
+                        activateAuth(res.accessToken, res.refreshToken, res.roles[0]);
+                        // console.log(res);
+                        registerEsal(esal);
+                    }),
+            );
+        }
+    });
 };
 
 export const EsalService = {
-  regsiterBunch,
+    registerBunch,
 };
--- frontend/src/infrastructure/repositories/ESAL.repository.ts
+++ frontend/src/infrastructure/repositories/ESAL.repository.ts
@@ -1,38 +1,61 @@
-import { Esal, EsalEmployee } from '../../domain/models/Esal';
-import { BASE } from '../base';
-import { EsalDTO, EsalEmployeeDTO } from '../http/dtos/EsalDTO';
-import { http } from '../http/http';
-import { ROUTE } from '../http/routes';
+import {Esal, EsalEmployee} from '../../domain/models/Esal';
+import {BASE} from '../base';
+import {EsalDTO, EsalEmployeeDTO} from '../http/dtos/EsalDTO';
+import {http} from '../http/http';
+import {ROUTE} from '../http/routes';
 
+const registerEmployee = (newEmployee: EsalEmployee) => {
+    const esalEmployeeDTO: EsalEmployeeDTO = {
+        name: newEmployee.name,
+        surname: newEmployee.surname,
+        phoneNumber: newEmployee.phoneNumber,
+        email: newEmployee.email,
+        password: newEmployee.password,
+    };
+    return new Promise((resolve, reject) =>
+        http
+            .post(
+                `${BASE.API}/${ROUTE.API.organizations.registerEsalEmployee}`,
+                JSON.stringify(esalEmployeeDTO),
+                'application/json',
+                false,
+            )
+            .then((response) => {
+                resolve(response);
+            }),
+    );
+};
 const registerEsal = (newEsal: Esal) => {
-  const newEsalDTO: EsalDTO = {
-    name: newEsal.name,
-    description: newEsal.description,
-    website: newEsal.website,
-    registeredEntity: newEsal.registeredEntity,
-    entityType: newEsal.entityType,
-    privacyPolicy: newEsal.privacyPolicy,
-    dataProtectionPolicy: newEsal.dataProtectionPolicy,
-    island: newEsal.island,
-    zipCode: newEsal.zipCode,
-  };
-  return new Promise((resolve, reject) => {
-    http
-      .post(
-        `${BASE.API}/${ROUTE.API.organizations.register}`,
-        JSON.stringify(newEsalDTO),
-        'application/json',
-        true,
-      )
-      .then((response) => {
-        resolve(response);
-      })
-      .catch((rej) => reject(rej));
-  });
+    const newEsalDTO: EsalDTO = {
+        name: newEsal.name,
+        description: newEsal.description,
+        website: newEsal.website,
+        registeredEntity: newEsal.registeredEntity,
+        entityType: newEsal.entityType,
+        privacyPolicy: newEsal.privacyPolicy,
+        dataProtectionPolicy: newEsal.dataProtectionPolicy,
+        island: newEsal.island,
+        zipCode: newEsal.zipCode,
+    };
+    return new Promise((resolve, reject) => {
+        http
+            .post(
+                `${BASE.API}/${ROUTE.API.organizations.register}`,
+                JSON.stringify(newEsalDTO),
+                'application/json',
+                true,
+            )
+            .then((response) => {
+                resolve(response);
+            })
+            .catch((rej) => reject(rej));
+    });
 };
 
-const uploadLogo = () => {};
+const uploadLogo = () => {
+};
+
 
 export const ESALRepository = {
-  registerEsal,
+    registerEsal, registerEmployee
 };
