2fb6e044fd4a1f978051e5aba04791d13b8e8841
Samuel Romero Arbelo <oiranca@gmail.com>
2021-04-27 10:25:46 +0100

    refactor:modified useCheckEsal.ts with the employee attributes

--- frontend/src/infrastructure/hooks/useCheckEsal/types.ts
+++ frontend/src/infrastructure/hooks/useCheckEsal/types.ts
@@ -1,13 +1,19 @@
-import { stateValidateTypes } from '../../view/components/atoms/InputFieldForm/types';
+import {stateValidateTypes} from '../../view/components/atoms/InputFieldForm/types';
 
 export interface CheckInterface {
-  name: stateValidateTypes;
-  description: stateValidateTypes;
-  website: stateValidateTypes;
-  entityType: stateValidateTypes;
-  registeredEntity: stateValidateTypes;
-  privacyPolicy: stateValidateTypes;
-  dataProtectionPolicy: stateValidateTypes;
-  island: stateValidateTypes;
-  zipCode: stateValidateTypes;
+    name: stateValidateTypes;
+    description: stateValidateTypes;
+    website: stateValidateTypes;
+    entityType: stateValidateTypes;
+    registeredEntity: stateValidateTypes;
+    privacyPolicy: stateValidateTypes;
+    dataProtectionPolicy: stateValidateTypes;
+    island: stateValidateTypes;
+    zipCode: stateValidateTypes;
+    nameEmployee: stateValidateTypes;
+    surname: stateValidateTypes;
+    phoneNumber: stateValidateTypes;
+    email: stateValidateTypes;
+    repeatEmail: stateValidateTypes;
+    password: stateValidateTypes;
 }
