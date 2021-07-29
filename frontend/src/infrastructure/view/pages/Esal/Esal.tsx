247035ce8f3f7e7319ac1378903e012f4f36635b
blueC4P <osamag4g@gmail.com>
2021-07-29 11:30:45 +0100

    feat: Create "Necesito Ayuda"

--- frontend/src/infrastructure/view/pages/Esal/Esal.tsx
+++ frontend/src/infrastructure/view/pages/Esal/Esal.tsx
@@ -2,10 +2,25 @@ import * as React from 'react';
 import './Esal.scss';
 import { FormRegisterEsal } from '../../components/organisms/Forms/FormRegisterEsal';
 
-export const Esal: React.FC<{}> = () => (
-  <div className="esal">
-    <FormRegisterEsal />
-  </div>
-);
+export const Esal: React.FC<{}> = () => {
+  return (
+    <div className="esal">
+      <h1>Necesito Ayuda</h1>
+      <div className="content">
+        ¿Eres un colectivo o entidad y necesitas apoyo de voluntariado para llevar a cabo tu iniciativa?, ¿eres una persona con alguna necesidad y necesitas voluntarios que te ayuden? ¡No dudes en escribirnos!
+
+        Esta plataforma ha sido creada justamente con este objetivo, el de ayudarnos entre todos para salir lo antes posible de este complicado momento que estamos viviendo.
+
+        Si tienes alguna duda consulta nuestras preguntas frecuentes.
+      </div>
+      <div className="buttons">
+        <button>ENTIDAD</button>
+        <button>PERSONA FÍSICA</button>
+      </div>
+      {/* <FormRegisterEsal /> */}
+    </div>
+
+  )
+};
 
 Esal.displayName = 'Esal';
