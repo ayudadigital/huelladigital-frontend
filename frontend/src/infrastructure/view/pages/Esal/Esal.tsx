9081984f17e28919033be7cc2451b3df5e12cacc
blueC4P <osamag4g@gmail.com>
2021-08-09 00:28:09 +0100

    Esal/Necesito Ayuda view styled

--- frontend/src/infrastructure/view/pages/Esal/Esal.tsx
+++ frontend/src/infrastructure/view/pages/Esal/Esal.tsx
@@ -7,15 +7,24 @@ export const Esal: React.FC<{}> = () => {
     <div className="esal">
       <h1>Necesito Ayuda</h1>
       <div className="content">
-        ¿Eres un colectivo o entidad y necesitas apoyo de voluntariado para llevar a cabo tu iniciativa?, ¿eres una persona con alguna necesidad y necesitas voluntarios que te ayuden? ¡No dudes en escribirnos!
-
-        Esta plataforma ha sido creada justamente con este objetivo, el de ayudarnos entre todos para salir lo antes posible de este complicado momento que estamos viviendo.
-
-        Si tienes alguna duda consulta nuestras preguntas frecuentes.
+        <p>
+          ¿Eres un colectivo o entidad y necesitas apoyo de voluntariado para llevar a cabo tu iniciativa?, ¿eres una persona con alguna necesidad y necesitas voluntarios que te ayuden? ¡No dudes en escribirnos!
+        </p>
+        <p>
+          Esta plataforma ha sido creada justamente con este objetivo, el de ayudarnos entre todos para salir lo antes posible de este complicado momento que estamos viviendo.
+        </p>
+        <p>
+          Si tienes alguna duda consulta nuestras preguntas frecuentes.
+        </p>
       </div>
       <div className="buttons">
         <button>ENTIDAD</button>
         <button>PERSONA FÍSICA</button>
+      </div>
+      <div className="last-message">
+        <h2>
+          Recuerda que juntos, somos más fuertes.
+        </h2>
       </div>
       {/* <FormRegisterEsal /> */}
     </div>
